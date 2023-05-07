f2256ca24b8bd82e01ae514de8cb62c155699dd6
Elvina Baalbergen <e.baalbergen@gmail.com>
2023-05-07 15:17:21 +0200

    Training iteration

--- source_code/models/BERT.py
+++ source_code/models/BERT.py
@@ -1,48 +1,78 @@
-from transformers import BertTokenizer, TFBertForSequenceClassification
-from sklearn.model_selection import train_test_split
-import random
+from transformers import BertTokenizer, TFBertForSequenceClassification, DataCollatorWithPadding
+import datasets 
 import numpy as np
 import sys
 import pickle
+import pandas as pd
 import tensorflow as tf
-
 
 sys.path.append("/home/niek/Elvina/Thesis/repo2/Fact_Checking_model/source_code/data_generation")
 from create_data_train import save, load, Chunk, Pair
 
 PAIR_CONSISTENT_PATH = './Fact_Checking_model/data/processed/Pair_Consistent.pkl'
+PAIR_UNRELATED_PATH = './Fact_Checking_model/data/processed/Pair_Unrelated.pkl'
+
+# Set tokenizer and model to global scope
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = TFBertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="tf")
 
 def main():  
-  # transfrom dataset for training
-  consistent_pairs = load(PAIR_CONSISTENT_PATH)[:20]
-  unrelated_pairs = load(PAIR_CONSISTENT_PATH)[:20]
+  # transfrom Piar to dataset for training
+  consistent_pairs = load(PAIR_CONSISTENT_PATH)[:10000]
+  unrelated_pairs = load(PAIR_UNRELATED_PATH)[:10000]
 
-  tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-  formatted_inputs = list(map(preprocess_pair, consistent_pairs)) + list(map(preprocess_pair, unrelated_pairs))
-  
-  preprocessed_consistent_pairs = dict(tokenizer(formatted_inputs, padding=True, truncation=True, return_tensors="tf"))
-  preprocessed_unrelated_pairs_label = tf.concat([tf.repeat(0, 20), tf.repeat(1, 20)],0) 
+  consistent_pairs_df = pairs_to_df(consistent_pairs, 0)
+  unrelated_pairs_df = pairs_to_df(unrelated_pairs, 1)
+  merged_df = pd.concat([consistent_pairs_df, unrelated_pairs_df])
 
-  print(preprocessed_consistent_pairs)
-  print(preprocessed_unrelated_pairs_label)
+  raw_dataset = datasets.Dataset.from_pandas(pd.DataFrame(data=merged_df))
+  tokenized_datasets = raw_dataset.map(preprocess)
 
-  '''
-  preprocessed_consistent_pairs_label = np.repeat(0, len(preprocessed_consistent_pairs))
-  preprocessed_unrelated_pairs = dict(map(preprocess_pair, unrelated_pairs))
-  preprocessed_unrelated_pairs_label = np.repeat(2, len(preprocessed_consistent_pairs))
+  tf_train_dataset = tokenized_datasets.to_tf_dataset(
+    columns=["attention_mask", "input_ids", "token_type_ids"],
+    label_cols=["Label"],
+    shuffle=True,
+    collate_fn=data_collator,
+    batch_size=4,
+  )
 
-  X = preprocessed_consistent_pairs + preprocessed_unrelated_pairs
-  y = np.concatenate([preprocessed_consistent_pairs_label, preprocessed_unrelated_pairs_label])  
-  '''
-  dataset = tf.data.Dataset.from_tensor_slices((preprocessed_consistent_pairs, preprocessed_unrelated_pairs_label))
+  # fit model
+  num_epochs = 3
+  num_train_steps = len(tf_train_dataset) * num_epochs
 
-  # build model
-  optimizer = tf.keras.optimizers.Adam(learning_rate=3e-5)
-  loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-  model = TFBertForSequenceClassification.from_pretrained('bert-base-uncased')
-  model.compile(optimizer=optimizer, loss=loss)
-  model.fit(preprocessed_consistent_pairs, preprocessed_unrelated_pairs_label)
+  lr_scheduler = tf.keras.optimizers.schedules.PolynomialDecay(
+    1e-5,
+    decay_steps=num_train_steps,
+    end_learning_rate=0.0,
+)
 
+  optimizer = tf.keras.optimizers.Adam(learning_rate=lr_scheduler)
+
+  model.compile(
+    optimizer= optimizer,
+    loss= tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+  )
+
+  model.fit(
+    tf_train_dataset,
+    epochs=num_epochs
+  ) 
+
+
+def pairs_to_df(pairs, label):
+  row_list = []
+ 
+  for pair in pairs:
+      newrow = {"Sentence":pair.Sentence, "Chunk":pair.Chunk, "Label":label}
+      row_list.append(newrow)
+
+  df = pd.DataFrame(row_list, columns=['Sentence','Chunk',"Label"]) 
+  return df
+
+def preprocess(row):
+  return tokenizer(row["Sentence"], row["Chunk"], truncation=True)
 
 def preprocess_pair(pair):
   input_text = "[CLS]" + pair.Chunk + "[SEP]" + pair.Sentence + "[SEP]"
